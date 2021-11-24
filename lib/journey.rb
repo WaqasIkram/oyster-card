ed82196dcc92a51dfbda045bf07c654db6c9e1f5
WaqasIkram <waqaschib@gmail.com>
2021-11-24 15:49:41 +0000

    Journey class with entry/exit station and without touch in/out feature

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,18 @@
 class Journey
 
+  attr_reader :entry_station
+
+  def initialize(entry_station=nil)
+    @entry_station = entry_station
+    @exit_station 
+
+  end
+
+  def exit_station(station=@exit_station)
+    @exit_station = station
+  end
+
+  def complete?
+    @entry_station != nil && @exit_station != nil
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,35 @@
 require "journey"
 
 describe Journey do
+  let(:station) {double :station}
+  let(:station2) {double :station2}
 
+  it 'should be able to store an entry station' do
+    journey = Journey.new(station)
+    expect(journey.entry_station).to eq(station)
+  end
+
+  it 'should be able to store an exit station' do
+    journey = Journey.new(station)
+    journey.exit_station(station2)
+    expect(journey.exit_station).to eq(station2)
+  end
+
+  it 'should be able to know if a journey is complet' do
+    journey = Journey.new(station)
+    journey.exit_station(station2)
+    expect(journey.complete?).to be true
+  end
+
+  it 'should be able to know if a journey is incomplete(i.e.no touched out)' do
+    journey = Journey.new(station)
+    expect(journey.complete?).to be false
+  end
+
+  it 'should be able to know if a journey is incomplete(i.e.no touched in)' do
+    journey = Journey.new
+    journey.exit_station(station2)
+    expect(journey.complete?).to be false
+  end
+  
 end
