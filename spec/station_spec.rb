0aaee3280b989b3e99a616f155d295713ae2e72a
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-24 15:01:52 +0000

    Added simple station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,12 @@
+require "station"
+
+describe Station do
+  it "should have a zone" do
+    station = Station.new(1, "Aldgate")
+    expect(station.zone).to be_a_kind_of(Integer)
+  end
+  it "should have a name" do
+    station = Station.new(1, "Aldgate")
+    expect(station.name).to be_a_kind_of(String)
+  end
+end
